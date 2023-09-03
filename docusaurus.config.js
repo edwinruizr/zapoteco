6f8bfc7d7937e9d3b01b0613aa1f4073d9c67fa8
Edwin Ruiz <edwinruizr1992@gmail.com>
2023-09-02 18:47:55 -0700

    trailingSlash: false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'edwinruizr', // Usually your GitHub org/user name.
   projectName: 'zapoteco', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
