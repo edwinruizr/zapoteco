df98e19f3ee98c33e20b6abc6a8a3c195090a2ca
Edwin Ruiz <edwinruizr1992@gmail.com>
2023-09-04 18:35:40 -0700

    add background img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
-import Translate from "@docusaurus/Translate";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,15 +12,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/alphabet">
-              <Translate id='learn.the.alphabet'>
-                  Learn the alphabet
-              </Translate>
-          </Link>
-        </div>
+        <div className={styles.buttons}/>
       </div>
     </header>
   );
