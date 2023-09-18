1523f28a39adb5e7738ff89824e3130c761fce3e
Edwin Ruiz <edwinruizr1992@gmail.com>
2023-09-17 19:19:25 -0700

    add footer link & fmt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,62 +1,61 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Zapoteco',
-  tagline: 'Learn Zapoteco San Francisco Cajonos (Ljxill) variation',
-  favicon: 'img/favicon.ico',
+  title: "Zapoteco",
+  tagline: "Learn Zapoteco San Francisco Cajonos (Ljxill) variation",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://edwinruizr.github.io',
+  url: "https://edwinruizr.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/zapoteco/',
+  baseUrl: "/zapoteco/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'edwinruizr', // Usually your GitHub org/user name.
-  projectName: 'zapoteco', // Usually your repo name.
+  organizationName: "edwinruizr", // Usually your GitHub org/user name.
+  projectName: "zapoteco", // Usually your repo name.
   trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: "en",
+    locales: ["en", "es"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         blog: false,
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/edwinruizr/zapoteco/edit/main/',
+          editUrl: "https://github.com/edwinruizr/zapoteco/edit/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-ERKC49M9VM',
+          trackingID: "G-ERKC49M9VM",
           anonymizeIP: true,
         },
         sitemap: {
-          changefreq: 'weekly',
-            priority: 0.6,
-            ignorePatterns: [],
-          filename: 'sitemap.xml',
+          changefreq: "weekly",
+          priority: 0.6,
+          ignorePatterns: [],
+          filename: "sitemap.xml",
         },
       }),
     ],
@@ -66,73 +65,81 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/social-card.png',
-      metadata: [{name: 'keywords', content: 'learn, zapoteco, san francisco cajonos, ljxill, oaxaca'}],
+      image: "img/social-card.png",
+      metadata: [
+        {
+          name: "keywords",
+          content: "learn, zapoteco, san francisco cajonos, ljxill, oaxaca",
+        },
+      ],
       navbar: {
-        title: 'Zapoteco',
+        title: "Zapoteco",
         logo: {
-          alt: 'Zapoteco Logo',
-          src: 'img/zapoteco.png',
+          alt: "Zapoteco Logo",
+          src: "img/zapoteco.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
           },
           {
-            type: 'localeDropdown',
-            sidebarId: 'locale',
+            type: "localeDropdown",
+            sidebarId: "locale",
             position: "right",
           },
           {
-            href: 'https://github.com/edwinruizr/zapoteco',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/edwinruizr/zapoteco",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Alphabet',
-                to: '/docs/basics/alphabet',
+                label: "Alphabet",
+                to: "/docs/basics/alphabet",
               },
               {
-                label: 'Numbers',
-                to: '/docs/basics/numbers'
+                label: "Numbers",
+                to: "/docs/basics/numbers",
               },
               {
-                label: 'Flashcards',
-                to: '/docs/basics/flashcards',
+                label: "Flashcards",
+                to: "/docs/basics/flashcards",
+              },
+              {
+                label: "Healing Herbs",
+                to: "/docs/herbs",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/sanfranciscocajonosla/',
+                label: "Instagram",
+                href: "https://www.instagram.com/sanfranciscocajonosla/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/edwinruizr/zapoteco',
+                label: "GitHub",
+                href: "https://github.com/edwinruizr/zapoteco",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
