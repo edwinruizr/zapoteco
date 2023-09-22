864d0acf761c20aaa163a297ace2378b954b8814
Edwin Ruiz <edwinruizr1992@gmail.com>
2023-09-21 22:02:23 -0700

    Deploy website - based on dd8112621be835aface9e6548818fcc6a17e4d5b

--- es/assets/js/434.da5b1640.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkzapoteco=self.webpackChunkzapoteco||[]).push([[434],{5434:(t,a,e)=>{e.d(a,{H6t:()=>c});var h=e(8357);function c(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 14H8V8h8v8z"}}]})(t)}}}]);
